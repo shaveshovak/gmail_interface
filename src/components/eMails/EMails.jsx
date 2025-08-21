731955bfcddb02c7822ac876244ec88ba601deed
MarcelSingh <Marcel.singh@rocketmail.com>
2025-08-21 11:50:40 +0200

    added

---
+++ src/components/eMails/EMails.jsx
@@ -0,0 +1,7 @@
+
+
+const EMails = () => {
+    return(
+
+    )
+};
--- src/components/mainContainer/MainContainer.jsx
+++ src/components/mainContainer/MainContainer.jsx
@@ -1,14 +1,37 @@
-import { faRotateRight } from "@fortawesome/free-solid-svg-icons";
+import { faRotateRight, faInbox, faUsers, faTag} from "@fortawesome/free-solid-svg-icons";
 import styles from "./mainContainer.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ListItem from "../list-item/ListItem";
 
 const MainContainer = () => {
   return (
     <div className={styles.main_container}>
+      <div className={styles.refresh}>
         <input type="checkbox" />
-        <FontAwesomeIcon icon={faRotateRight}/>
+        <FontAwesomeIcon icon={faRotateRight} />
+      </div>
+      <div className={styles.link_box}>
+        <ListItem
+          title={"Primary"}
+          icon={faInbox}
+          classes={styles.message_links}
+        />
+        <ListItem
+          title={"Social"}
+          icon={faUsers}
+          classes={styles.message_links}
+        />
+        <ListItem
+          title={"Promotions"}
+          icon={faTag}
+          classes={`${styles.message_links} ${styles.active}`}
+        />
+      </div>
+      <div>
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainContainer
+export default MainContainer;
